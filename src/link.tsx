ed92fb6f7b884bb3540355bc8f1bac9667aa502a
Konrad Guzek <52281528+kguzek@users.noreply.github.com>
2025-11-08 17:47:23 +0100

    feat(link): allow navigation prevention (#6)

--- src/link.tsx
+++ src/link.tsx
@@ -3,63 +3,42 @@ import NextLink from "next/link";
 import { useCallback } from "react";
 import { useTransitionRouter } from "./use-transition-router";
 
-// copied from https://github.com/vercel/next.js/blob/66f8ffaa7a834f6591a12517618dce1fd69784f6/packages/next/src/client/link.tsx#L180-L191
-function isModifiedEvent(event: React.MouseEvent): boolean {
-  const eventTarget = event.currentTarget as HTMLAnchorElement | SVGAElement;
-  const target = eventTarget.getAttribute("target");
-  return (
-    (target && target !== "_self") ||
-    event.metaKey ||
-    event.ctrlKey ||
-    event.shiftKey ||
-    event.altKey || // triggers resource download
-    (event.nativeEvent && event.nativeEvent.which === 2)
-  );
-}
-
-// copied from https://github.com/vercel/next.js/blob/66f8ffaa7a834f6591a12517618dce1fd69784f6/packages/next/src/client/link.tsx#L204-L217
-function shouldPreserveDefault(
-  e: React.MouseEvent<HTMLAnchorElement>,
-): boolean {
-  const { nodeName } = e.currentTarget;
-
-  // anchors inside an svg have a lowercase nodeName
-  const isAnchorNodeName = nodeName.toUpperCase() === "A";
-
-  if (isAnchorNodeName && isModifiedEvent(e)) {
-    // ignore click for browser’s default behavior
-    return true;
-  }
-
-  return false;
-}
-
-// This is a wrapper around next/link that explicitly uses the router APIs
-// to navigate, and trigger a view transition.
-
+/**
+ * This is a wrapper around next/link that explicitly uses the router APIs
+ * to navigate, and trigger a view transition.
+ */
 export function Link<T>(props: LinkProps<T>) {
   const router = useTransitionRouter();
 
   const { href, as, replace, scroll } = props;
-  const onClick = useCallback(
-    (e: React.MouseEvent<HTMLAnchorElement>) => {
-      if (props.onClick) {
-        props.onClick(e);
-      }
+  const onNavigate = useCallback(
+    ({ preventDefault }: { preventDefault: () => void }) => {
+      // adapted from https://github.com/vercel/next.js/blob/4f3980953800258d654ece9a239734e77950fce6/packages/next/src/client/link.tsx#L242-L254
+      if (props.onNavigate) {
+        let isDefaultPrevented = false;
 
-      if ("startViewTransition" in document) {
-        if (shouldPreserveDefault(e)) {
+        props.onNavigate({
+          preventDefault: () => {
+            isDefaultPrevented = true;
+            preventDefault();
+          },
+        });
+
+        if (isDefaultPrevented) {
           return;
         }
-
-        e.preventDefault();
-
-        const navigate = replace ? router.replace : router.push;
-        navigate(as || href, { scroll: scroll ?? true });
       }
+
+      if (!("startViewTransition" in document)) {
+        return;
+      }
+      preventDefault();
+
+      const navigate = replace ? router.replace : router.push;
+      navigate(as || href, { scroll: scroll ?? true });
     },
-    [props.onClick, href, as, replace, scroll],
+    [props.onNavigate, href, as, replace, scroll],
   );
 
-  return <NextLink {...props} onClick={onClick} />;
+  return <NextLink {...props} onNavigate={onNavigate} />;
 }
